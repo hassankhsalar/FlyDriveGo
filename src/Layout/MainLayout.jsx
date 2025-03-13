6a4d4bf2f8813bc2c1e7c1902b2cd3f54778d7a3
Hassan kh salar <khsalarhassan@gmail.com>
2025-03-13 17:38:38 +0600

    added Navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Shared/Navbar";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
+  return (
+    <section>
+        <div className="h-14 border-2">
+            <Navbar></Navbar>
         </div>
-    );
+      <div className="min-h-screen">
+        <Outlet></Outlet>
+      </div>
+    </section>
+  );
 };
 
 export default MainLayout;
