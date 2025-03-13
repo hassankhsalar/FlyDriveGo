0082513745816fd40abcdd4c70e62c8b346ebb4f
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-03-13 10:25:13 -0700

    rahat: added hero section and added a demo section of tranportation section with filtering functionality

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import {
   } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import TransportationOptions from "../Pages/Transportation/TransportationOptions";
 
   export const router = createBrowserRouter([
     {
@@ -13,7 +14,11 @@ import Home from "../Pages/Home/Home";
         {
             path: '/',
             element: <Home></Home>
-        }
+        },
+        {
+            path: '/transportation',
+            element: <TransportationOptions></TransportationOptions>
+        },
       ]
     },
   ]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Catamaran', 'serif'],
+        "red-rose": ['"Red Rose"', "serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
       colors: {
-        'text': '#03191c',
-        'background': '#ffffff',
-        'primary': '#4EDAE4',
-        'secondary': '#DDF2D1',
-        'SmokeWhite': '##F5F5F5',
-        'CharcoleDark' : '#333333',
-       },
+        text: "#03191c",
+        background: "#ffffff",
+        primary: "#4EDAE4",
+        secondary: "#DDF2D1",
+        SmokeWhite: "#F5F5F5",
+        CharcoleDark: "#333333",
+      },
     },
-  }, 
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+  },
+  plugins: [require("daisyui")],
+};
