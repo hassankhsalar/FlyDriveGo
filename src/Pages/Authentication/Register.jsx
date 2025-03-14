73269525d0de7ba8dceb5c934c3ff56c4cebfba2
SA Al Walid <saawalid03@gmail.com>
2025-03-14 22:26:36 +0600

    register section added

---
+++ src/Pages/Authentication/Register.jsx
@@ -0,0 +1,182 @@
+import React, { useContext, useState } from "react";
+import { FaArrowLeft, FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import GoogleUserLogin from "../../Shared/GoogleUserLogin";
+import WelcomeBanner from "../../Shared/WelcomeBanner";
+import { AuthContext } from "../../Provider/AuthProvider";
+
+const Register = () => {
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
+
+  // React Hook Form setup
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  // Form submission handler
+  const onSubmit = async (data) => {
+    const { name, email, photo, password } = data;
+    createUser(email, password).then((res) => {
+      updateUserProfile({ displayName: name, photoURL: photo });
+      reset();
+      toast.success("User created successfully!");
+    });
+  };
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="w-full flex justify-center items-center">
+        <div className="card w-full md:max-w-[380px] lg:max-w-[440px] p-10 mt-6">
+          <div className="font-semibold mb-4">
+            <Link to="/" className="flex gap-2 text-center items-center">
+              <FaArrowLeft />
+              Home
+            </Link>
+          </div>
+          <h2 className="text-2xl font-bold text-left">Register</h2>
+          <p className="text-left font-semibold mb-2">
+            Already have an account?{" "}
+            <Link to="/login" className="text-[#023E8A] pl-1">
+              Login
+            </Link>
+          </p>
+
+          <form onSubmit={handleSubmit(onSubmit)} className="pb-3">
+            {/* Name */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-gray-700 font-semibold">
+                  Name
+                </span>
+              </label>
+              <input
+                {...register("name", { required: "Name is required" })}
+                type="text"
+                placeholder="Enter your name"
+                className="input input-bordered"
+              />
+              {errors.name && (
+                <span className="text-red-600 text-sm">
+                  {errors.name.message}
+                </span>
+              )}
+            </div>
+
+            {/* Image URL */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-gray-700 font-semibold">
+                  Image URL
+                </span>
+              </label>
+              <input
+                {...register("photo", {
+                  required: "Image URL is required",
+                  pattern: {
+                    value: /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg))$/i,
+                    message: "Enter a valid image URL",
+                  },
+                })}
+                type="text"
+                placeholder="Enter image URL"
+                className="input input-bordered"
+              />
+              {errors.photo && (
+                <span className="text-red-600 text-sm">
+                  {errors.photo.message}
+                </span>
+              )}
+            </div>
+
+            {/* Email */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-gray-700 font-semibold">
+                  Email
+                </span>
+              </label>
+              <input
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Enter a valid email",
+                  },
+                })}
+                type="email"
+                placeholder="Enter your email"
+                className="input input-bordered"
+              />
+              {errors.email && (
+                <span className="text-red-600 text-sm">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+
+            {/* Password */}
+            <div className="form-control relative">
+              <label className="label">
+                <span className="label-text text-gray-700 font-semibold">
+                  Password
+                </span>
+              </label>
+              <input
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                  pattern: {
+                    value:
+                      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{6,}$/,
+                    message:
+                      "Must include uppercase, lowercase, number & special character",
+                  },
+                })}
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter password"
+                className="input input-bordered"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="btn btn-xs absolute right-4 bottom-[13px]"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+              {errors.password && (
+                <span className="text-red-600 text-sm">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+
+            {/* Register */}
+            <div className="form-control mt-6">
+              <button type="submit" className="btn bg-[#023E8A] text-white">
+                Register
+              </button>
+            </div>
+          </form>
+
+          <div>
+            <GoogleUserLogin />
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <WelcomeBanner />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home";
 
 import TransportationOptions from "../Pages/Transportation/TransportationOptions";
 import Login from "../Pages/Authentication/Login";
+import Register from "../Pages/Authentication/Register";
 
 export const router = createBrowserRouter([
   {
@@ -17,12 +18,15 @@ export const router = createBrowserRouter([
       {
         path: "/transportation",
         element: <TransportationOptions></TransportationOptions>,
-        },
-        {
+      },
+      {
         path: "login",
         element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
     ],
   },
 ]);
-
