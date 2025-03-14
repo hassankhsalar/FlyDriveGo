16c61dea2402e5031791b4ee62319c0d80fc03f7
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-03-14 04:47:46 -0700

    added the error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,21 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
-<<<<<<< HEAD
 import Transportation from "../Pages/Transportation/Transportation";
+import ErrorPage from "../components/ErrorElements/ErrorPage";
 
-=======
-
-import TransportationOptions from "../Pages/Transportation/TransportationOptions";
-import Login from "../Pages/Authentication/Login";
-
-
-
->>>>>>> a31d2e9ba3908c65f521a351e9e6c47d1e9a9d45
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
@@ -23,21 +16,8 @@ export const router = createBrowserRouter([
       },
       {
         path: "/transportation",
-<<<<<<< HEAD
         element: <Transportation></Transportation>,
-=======
-        element: <TransportationOptions></TransportationOptions>,
-        },
-        {
-        path: "login",
-        element: <Login />,
->>>>>>> a31d2e9ba3908c65f521a351e9e6c47d1e9a9d45
       },
     ],
   },
 ]);
-<<<<<<< HEAD
-=======
-
-
->>>>>>> a31d2e9ba3908c65f521a351e9e6c47d1e9a9d45
