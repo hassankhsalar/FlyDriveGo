e2c0f90f02cdc21157c1cbf83fa53f6ce5f8c304
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-03-21 02:13:09 -0700

    Optimized the Gemini Chatbot

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./Provider/AuthProvider.jsx";
+import { LoadingProvider } from "./contexts/LoadingContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster />
+      <LoadingProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </LoadingProvider>
     </AuthProvider>
   </StrictMode>
 );
