c9bfaf77ae23fee9abe0c1ca287da0137cc12ece
noorjahan220 <noorjahanmeem220@gmail.com>
2025-03-24 02:02:58 +0600

    added the testimonial,trusstedPartners section in tour-pack page

--- src/Pages/TourPackages/TourPackages.jsx
+++ src/Pages/TourPackages/TourPackages.jsx
@@ -4,6 +4,9 @@ import Cards from "./components/Cards";
 import { Link } from "react-router-dom";
 import Ecommerce from "./Ecommerce";
 
+import Testimonial from "./Testimonial";
+import TrustedPartners from "../Home/TrustedPartners";
+
 const TourPackages = () => {
     const [tourData, setTourData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -32,7 +35,9 @@ const TourPackages = () => {
             <div>
                 <HeroSection />
             </div>
-
+            <div>
+                <TrustedPartners/>
+            </div>
             {/* Card Section */}
             <div className="my-10">
                 <h2 className="font-extrabold text-3xl text-center mb-10" style={{ color: '#4EDAE4' }}>
@@ -51,6 +56,28 @@ const TourPackages = () => {
             <div className="my-10 p-2">
                 <Ecommerce />
             </div>
+            {/* testimonial */}
+            <Testimonial />
+            {/* CTA Section */}
+            <div className="mt-16 w-full h-full md:h-[400px] bg-[url('/cta.jpg')] bg-cover bg-center bg-no-repeat flex flex-col md:flex-row justify-center items-center py-8 px-4 md:px-8 lg:px-16">
+                {/* Left Section - Heading */}
+                <div className="w-full md:w-1/2 text-center md:text-left">
+                    <h1 className="font-red-rose text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-6 md:mb-14">
+                        Ready to Explore? <br /> Book Your Next Adventure Today!
+                    </h1>
+                </div>
+
+                {/* Right Section - Description and Button */}
+                <div className="w-full md:w-1/2 flex flex-col gap-4 items-center md:items-start">
+                    <p className="font-poppins font-normal text-base md:text-lg text-white text-center md:text-left">
+                        From flights to tour packages and travel essentials, everything you need is just a click away. Start planning now and make your journey unforgettable!
+                    </p>
+                    <button className="bg-[#4EDAE4] text-white font-semibold px-6 py-3 rounded-lg hover:bg-[#3cb8c6] transition-all duration-300">
+                        Explore The World
+                    </button>
+                </div>
+            </div>
+            
         </div>
     );
 };
