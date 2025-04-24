73af0c43043a7f1a1bfe1772ab2c534b86353f40
SA Al Walid <saawalid03@gmail.com>
2025-04-25 00:26:36 +0600

    feat: baseUrl change for testing

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'https://fly-drive-go-server.vercel.app',
-   
-  });
+  // baseURL: 'https://fly-drive-go-server.vercel.app',
+  baseURL: "http://localhost:5000",
+});
 
 const useAxiosPublic = () => {
   return axiosPublic;
